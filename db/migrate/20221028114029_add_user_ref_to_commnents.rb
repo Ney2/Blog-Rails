ba1ffce33c90b053d8138246861d02953b6bd2a2
Ney2 <kidistdinku5057@gmail.com>
2022-10-28 15:30:53 +0300

    Add validation and test for the models

--- db/migrate/20221028114029_add_user_ref_to_commnents.rb
+++ db/migrate/20221028114029_add_user_ref_to_commnents.rb
@@ -1,4 +1,4 @@
-class AddUserRefToComments < ActiveRecord::Migration[7.0]
+class AddUserRefToCommnents < ActiveRecord::Migration[7.0]
   def change
     add_reference :comments, :author, null: false, foreign_key: { to_table: :users }
     add_reference :comments, :post, null: false, foreign_key: true
