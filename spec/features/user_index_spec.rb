ebf77029faa13eb53ab1274053216862d49d01f2
abdollahraafat <abdollah.raafat.daif@gmail.com>
2022-11-09 13:17:43 +0200

    Test the index spec

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -7,17 +7,17 @@ RSpec.describe User, type: :feature do
       @users = User.all
     end
 
-    # it 'should see the username of all other users' do
-    #   @users.each do |user|
-    #     expect(page).to have_content(user.name)
-    #   end
-    # end
+    it 'should see the username of all other users' do
+      @users.each do |user|
+        expect(page).to have_content(user.name)
+      end
+    end
 
-    # it 'should see profile picture for each user' do
-    #   @users.each do |user|
-    #     expect(page).to have_css("img[src*='#{user.photo}']")
-    #   end
-    # end
+    it 'should see profile picture for each user' do
+      @users.each do |user|
+        expect(page).to have_css("img[src*='#{user.photo}']")
+      end
+    end
 
     it 'should see the number of posts each user has written' do
       @users.each do |user|
