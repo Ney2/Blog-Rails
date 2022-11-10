e1df9c39ade089390203fa6fc97d1a0bf65a2825
Ney2 <kidistdinku5057@gmail.com>
2022-11-10 23:09:58 +0300

    Update file to add route to the endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root to: "users#index"
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :likes, only: [:create]
-      resources :comments, only: [:new, :create]
+  namespace :api do
+    namespace :v1 do
+      post :auth, to: 'authentication#create'
+      resources :users, only: [] do
+        resources :posts, only: [:index] do
+          resources :comments, only: [:index, :create]
+        end
+      end
     end
-end
+  end
+
+  resources :users, only: [:index, :show] do
+    resources :posts, except: [:update, :edit] do
+      resources :comments, only: [:new, :create, :destroy]
+      resources :likes, only: [:create]
+    end
+  end
+
+  root 'users#index'
 end
